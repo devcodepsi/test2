0f6abc67581bdef23431e2270e6ffa00a16fcc2d
parksangil <devcodepsi@gmail.com>
2023-04-06 16:09:16 +0900

    index

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 import LayoutDefault from "../../layouts/LayoutDefault";
 
 const Index = () => {
     return (
         <LayoutDefault>
+            <Link to="/product">Product</Link>
+            <Link to="/ids">Ids</Link>
         </LayoutDefault>
     )
 }
